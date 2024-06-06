5ab3673f0dfbd9b11ca9b0781ee4f78e79f15f94
marufmia95 <md.marufmia4768@gmail.com>
2024-06-06 11:16:56 +0600

    Update

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+// Database Connection
+mongoose.connect("mongodb+srv://ecommerce_bk:ecommerce@cluster0.ihlvv5z.mongodb.net/e-commerce", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Database connected successfully'))
+.catch(err => console.error('Database connection error: ', err));
+
+// API Creation
+app.get('/', (req, res) => {
+    res.send("Express App is Running");
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
