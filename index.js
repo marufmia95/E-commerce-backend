643b890a5418f0f32ca05d35bbae0f7a235ab956
marufmia95 <md.marufmia4768@gmail.com>
2024-06-09 14:35:20 +0600

    Update

--- index.js
+++ index.js
@@ -42,10 +42,35 @@ const product = mongoose.model('Product',{
     id:{
         type:Number,
         required:true,
-    }
+    },
     name:{
         type:String,
-    }
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        required:Date.now,
+    },
+    avilabl:{
+        type:Boolean,
+        required:true,
+    },
 })
 
 
